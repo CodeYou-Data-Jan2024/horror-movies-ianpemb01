f7cb120b84799dafded4a221a4f2201d9980e419
ianpemb01 <ianpemb01@gmail.com>
2024-06-19 21:22:27 -0400

    Homework upload.

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,12 @@
 -- Add your SQL here
+
+SELECT 
+    id AS 'Movie_Rating',
+    name AS 'Movie_Title',
+    imdb_rating AS 'Rating'
+FROM
+    movies
+WHERE year >= 1985
+    AND genre = 'horror'
+ORDER BY imdb_rating DESC
+LIMIT 3;
